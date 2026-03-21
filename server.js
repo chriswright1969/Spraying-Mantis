84629710abbb1e47db83a75a9acb4c4daf15172e
chriswright1969 <81012879+chriswright1969@users.noreply.github.com>
2026-03-21 05:32:46 +0000

    Update server.js

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ function getSite() {
     domain: env('DOMAIN', 'https://sprayingmantis.co.uk'),
     coverageArea: env('COVERAGE_AREA', 'Ellesmere, Shropshire and surrounding areas'),
     addressLine1: env('ADDRESS_LINE_1', 'Unit 7, Trench Farm'),
-    addressLine1: env('ADDRESS_LINE_1', 'The Trench'),
-    addressLine2: env('ADDRESS_LINE_3', 'Ellesmere, SY12 0LR'),
+    addressLine2: env('ADDRESS_LINE_1', 'The Trench'),
+    addressLine3: env('ADDRESS_LINE_3', 'Ellesmere, SY12 0LR'),
     openingHours: env('OPENING_HOURS', 'Mon-Fri 8:00am-5:00pm'),
     logo: env('DEFAULT_LOGO', '/public/logo2.png'),
     formRecipient: env('FORM_RECIPIENT', email)
